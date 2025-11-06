23ce71364b18ced376dd61321d98eb0edb22cfa7
BangBANG <2967094107@qq.com>
2025-11-06 12:33:18 +0800

    fixed ： 路由不够简洁

--- backend/internal/app/handler/user_handler.go
+++ backend/internal/app/handler/user_handler.go
@@ -9,7 +9,7 @@ import (
 func BasicFlag(r *gin.Engine) {
 	r.POST("/api/register", service.RegisterUser())
 	r.POST("/api/login", service.LoginUser())
-	e := r.Group("/logined", service.JWTAuth())
+	e := r.Use(service.JWTAuth())
 	e.POST("/update-password", service.UpdateUserPassword())
 	e.POST("/update-username", service.UpdateUserName())
 	e.POST("/api/add-flag", service.PostUserFlags())
